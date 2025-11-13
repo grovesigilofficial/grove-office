c3a4c881cb2cbd6f19f5195e30d412f7b7229d76
GroveSigilOfficial <grovesigilofficial@gmail.com>
2025-11-13 01:47:39 -0800

    Create login.jsx

---
+++ src/pages/login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center mt-10 text-grove-green">
+      <h1 className="text-2xl mb-4">Login / Signup</h1>
+      <p>(Supabase authentication coming soon)</p>
+    </div>
+  )
+}
