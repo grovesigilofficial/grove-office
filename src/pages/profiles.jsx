be83c5df7c3b71f36da74431d892ac621024612e
GroveSigilOfficial <grovesigilofficial@gmail.com>
2025-11-13 01:49:21 -0800

    Create profiles.jsx

---
+++ src/pages/profiles.jsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div className="p-10 text-grove-green">
+      <h1 className="text-3xl mb-2">Profile</h1>
+      <p>Editable user info and progress tracker will go here.</p>
+    </div>
+  )
+}
